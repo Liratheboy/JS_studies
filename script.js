4e53cbc2cd0a3f431c82ba0dbe2c82747709be4b
João Paulo Lira <76487819+Liratheboy@users.noreply.github.com>
2024-05-07 15:59:27 -0300

    relembrando

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("ola");
+let y = -1;
+let k = 0;
+
+console.log('\n');
+for(k; k <= 10; k++)
+{
+    if( k % 2 == 0 )
+        console.log(`O número ${k} é par!\n`);
+    else
+        console.log(`o número ${k} é ímpar!\n`);   
+    y++
+}
+
+console.log("Valor de y: "+ y+ '\n');
+
